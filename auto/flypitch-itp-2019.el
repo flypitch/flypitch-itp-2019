549973e589f97e8133544b92729e47d22bc0b769
Jesse Michael Han <jessemichaelhan@gmail.com>
2019-03-28 22:37:01 -0400

    Some progress

--- auto/flypitch-itp-2019.el
+++ auto/flypitch-itp-2019.el
@@ -3,12 +3,12 @@
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("lipics-v2019" "a4paper" "USenglish" "cleveref" "autoref")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -19,21 +19,17 @@
    (TeX-add-symbols
     "B"
     "lil"
+    "N"
     "lstlanguagefiles")
    (LaTeX-add-labels
     "sect:outline"
     "sect:fol"
+    "def-boolean-valued-structure"
     "sect:bvm"
     "def-p-name"
+    "poset-yoneda"
     "sec:org9e70de8"
     "sect:forcing"
-    "sect:ccc")
-   (LaTeX-add-color-definecolors
-    "keywordcolor"
-    "tacticcolor"
-    "commentcolor"
-    "symbolcolor"
-    "sortcolor"
-    "attributecolor"))
+    "sect:ccc"))
  :latex)
 
